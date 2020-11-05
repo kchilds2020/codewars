51ff1fbf1bd4709662e8193322302bc823daeb37
kchilds2020 <63356648+kchilds2020@users.noreply.github.com>
2020-11-05 11:28:13 -0600

    added josephus permutation

--- Permutation.h
+++ Permutation.h
@@ -8,6 +8,52 @@ class Permuation{
     
     vector<string> allPossibleCombos(string str){
         //
-
+        vector<string> temp;
+        return temp;
     }
+
+    vector<int> josephus(vector<int> items, int k){
+        vector<int> result;
+        int count = 1;
+        int i = 0;
+        while(items.size() > 0){
+            if(i >= items.size()){
+                i = 0;
+            }else{
+            if (count % k == 0)
+            {
+                cout << items[i] << endl;
+                result.push_back(items[i]);
+                items.erase(items.begin() + (i));
+                count = 1;
+                i--;
+                
+            }
+            else{
+            
+                count++;
+            }
+            i++;
+            }
+        }
+        return result;
+    }
+
+    /* BEST PRACTICE
+    
+    std::vector <int> josephus(std::vector <int> items, int k) {
+    std::vector< int > result;
+    int pos = k-1;
+    
+    while (items.size())
+    {
+        pos %= items.size();
+        result.push_back(items[pos]);
+        items.erase(items.begin()+pos);
+        pos += k-1;
+    }
+
+    return result;
+}
+     */
 };
--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@
 int main(){
     Permuation perm;
 
-    string temp = perm.allPossibleCombos("abc");
+    vector<int> temp = perm.josephus({1,2,3,4,5,6,7,8,9,10}, 3);
+    for(int i = 0; i < temp.size(); i++){
+        cout << temp[i] << ", ";
+    }
     return 0;
 }
